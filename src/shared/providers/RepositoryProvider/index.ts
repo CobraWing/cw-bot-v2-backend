a37a85beaecb298177f0707ff1c9f2b8ad91bfe5
Rafael Pivatto <rafael.pivatto@gmail.com>
2020-06-28 17:57:45 -0300

    add providers for repositories

--- src/shared/providers/RepositoryProvider/index.ts
+++ src/shared/providers/RepositoryProvider/index.ts
@@ -1,7 +1,23 @@
 import { container } from 'tsyringe';
 
+import IServersRepository from '@modules/servers/repositories/IServersRepository';
+import ServersRepository from '@modules/servers/repositories/typeorm/ServersRepository';
+
+import IConfigurationsRepository from '@modules/configurations/repositories/IConfigurationsRepository';
+import ConfigurationsRepository from '@modules/configurations/repositories/typeorm/ConfigurationsRepository';
+
 import ICategoriesRepository from '@modules/commands/repositories/ICategoriesRepository';
 import CategoriesRepository from '@modules/commands/repositories/typeorm/CategoriesRepository';
+
+container.registerSingleton<IServersRepository>(
+  'ServersRepository',
+  ServersRepository,
+);
+
+container.registerSingleton<IConfigurationsRepository>(
+  'ConfigurationsRepository',
+  ConfigurationsRepository,
+);
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
