d814fb623cb87e15cf7eae4eb054fa6cf6d6c2e7
Rafael Pivatto <rafael.pivatto@gmail.com>
2021-02-16 18:12:26 -0300

    add combo to whitelist in custom command

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router, Request, Response } from 'express';
 import categoriesRouter from '@modules/categories/routes/categories.routes';
 import customCommandRouter from '@modules/commands/routes/customCommand.routes';
 import serversRouter from '@modules/servers/routes/servers.routes';
+import channelsRouter from '@modules/servers/routes/channels.routes';
 import authorizationRouter from '@modules/authorizations/routes/authorization.routes';
 
 const routes = Router();
 
 routes.use('/servers', serversRouter);
+routes.use('/channels', channelsRouter);
 routes.use('/categories', categoriesRouter);
 routes.use('/customCommands', customCommandRouter);
 routes.use('/authorizations', authorizationRouter);
